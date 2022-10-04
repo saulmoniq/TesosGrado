1acb83c9cdb7742c2561d8feabc2281e31615a78
saulmoniq <97468464+saulmoniq@users.noreply.github.com>
2022-10-04 19:17:05 -0400

    creating models and basic templates

--- TEG/models.py
+++ TEG/models.py
@@ -1,3 +1,25 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+import datetime
+
 
 # Create your models here.
+
+#Tablas con contenido fijo - De soporte
+
+
+
+
+#Tablas variables
+class usuario (models.Model):
+    nombre = models.CharField(max_length=20)
+    fechaNacimiento = models.DateTimeField(blank=True, default=datetime.date.today)
+    cedula = models.IntegerField(max_length=8, default= 0)
+    telefono = models.IntegerField(max_length=8, default= 0)
+    correo = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    ubicacion = models.CharField(max_length=10)
+    #FK's
+    
+
+    
--- TEG/views.py
+++ TEG/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import path
+from . import views
+
+def home(request):
+    return render(request, 'home.html')
